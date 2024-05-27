84c5d37cb8d1d4f6fbae4a09ffc4c6aef61f2f67
tobuya <thomasobuya95@gmail.com>
2024-05-27 11:16:16 +0300

    Store IP address instead of user ID

--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -2,4 +2,9 @@ class UserSearch < ApplicationRecord
   belongs_to :search_term
 
   validates :ip_address, presence: true
+
+  def self.create_from_ip(ip, term)
+    search_term = SearchTerm.find_or_create_by(term: term)
+    create(search_term: search_term, ip_address: ip)
+  end
 end
