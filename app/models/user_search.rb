f3e316118a7f3d01d6231ad9b6fcca7862e91a45
tobuya <thomasobuya95@gmail.com>
2024-05-29 11:08:39 +0300

    Create a log_search class method and broadcast_search private method

--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -3,8 +3,26 @@ class UserSearch < ApplicationRecord
 
   validates :ip_address, presence: true
 
-  def self.create_from_ip(ip, term)
-    search_term = SearchTerm.find_or_create_by(term: term)
-    create(search_term: search_term, ip_address: ip)
+  after_create_commit :broadcast_search
+
+  def self.log_search(ip_address, term)
+    Rails.logger.debug "Attempting to log search term: #{term} for IP: #{ip_address}"
+    last_search = UserSearch.where(ip_address: ip_address).order(created_at: :desc).first
+    if last_search.nil? || !term.start_with?(last_search.search_term.term)
+      Rails.logger.debug "Creating new search term: #{term} for IP: #{ip_address}"
+      search_term = SearchTerm.find_or_create_by(term: term)
+      create(search_term: search_term, ip_address: ip_address)
+    else
+      Rails.logger.debug "Search term: #{term} is similar to the last searched term for IP: #{ip_address}"
+    end
+  end
+
+  private
+
+  def broadcast_search
+    Rails.logger.debug "Broadcasting to search_analytics_channel with term: #{self.search_term.term} and IP: #{self.ip_address}"
+    ActionCable.server.broadcast("search_analytics_channel", { search_term: self.search_term.term, ip_address: self.ip_address })
+  rescue => e
+    Rails.logger.error "Broadcast failed: #{e.message}"
   end
 end
