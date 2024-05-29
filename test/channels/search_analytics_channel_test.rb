580b8374f7f3c11d468d5014f2ad63c2085468b3
tobuya <thomasobuya95@gmail.com>
2024-05-29 11:46:31 +0300

    Create initial test file for SearchAnalyticsChannel

---
+++ test/channels/search_analytics_channel_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class SearchAnalyticsChannelTest < ActionCable::Channel::TestCase
+  # test "subscribes" do
+  #   subscribe
+  #   assert subscription.confirmed?
+  # end
+end
