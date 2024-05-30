2b130dbdb6dfeaba76fbeed125e8daf86dff806f
tobuya <thomasobuya95@gmail.com>
2024-05-30 10:52:41 +0300

    Add babel-loader configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,26 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
 environment.config.entry = {
     application: './app/javascript/packs/application.js'
-}
+};
 
 environment.config.node = {
     __dirname: false,
     __filename: false
-  }
+};
 
-module.exports = environment
+// Add babel-loader configuration
+const babelLoader = {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env']
+        }
+    }
+};
+
+environment.loaders.append('babel', babelLoader);
+
+module.exports = environment;
