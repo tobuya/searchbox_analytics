fba61d65cf1b89890514d7a01c959b1bde397ec5
tobuya <thomasobuya95@gmail.com>
2024-05-31 09:31:11 +0300

    Seed the database with sample articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 SearchTerm.destroy_all
 
 SearchTerm.create([
-  { term: 'Hello world how are you?' },
-  { term: 'How is emil hajric doing' },
+  { term: 'Hello world, how are you?' },
+  { term: 'How is Emil Hajric doing' },
   { term: 'What is a good car' },
-  { term: 'Hello my name is Thomas and I would love to work at Helpjuice' }
+  { term: 'Thomas here, would love to work at Helpjuice' },
+  { term: 'Rails, PostgreSQL, and Vanilla JS.' }
 ])
